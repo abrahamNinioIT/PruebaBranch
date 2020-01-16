2779a757814504ff5dd8651416551a3ba2983a57
DAW <DAW@GEST-PC-01>
2020-01-16 19:42:30 +0100

    Primera mopdificacion de branch

--- src/PruebaCommit/PruebaCommit.java
+++ src/PruebaCommit/PruebaCommit.java
@@ -4,5 +4,6 @@ public class PruebaCommit {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		//Otra modificacion
+		System.out.println("Primera modificacion para branch");
 	}
 }
